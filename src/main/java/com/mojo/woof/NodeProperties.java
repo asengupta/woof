cbc7ce91c3e1747c93175e94c7ca6271bdced90a
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-07-07 06:17:43 +0530

    Adding some more constants for dependency tracking

--- src/main/java/com/mojo/woof/NodeProperties.java
+++ src/main/java/com/mojo/woof/NodeProperties.java
@@ -1,7 +1,10 @@
 package com.mojo.woof;
 
 public class NodeProperties {
+    public static String ID = "id";
     public static String TEXT = "text";
     public static String TYPE = "type";
     public static String FLOW_ID = "flowID";
+    public static String NAME = "name";
+    public static String LEVEL = "level";
 }
