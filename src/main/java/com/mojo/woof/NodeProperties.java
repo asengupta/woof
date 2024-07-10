cf990aa31129b5a27f67d9df34775f341e03e08b
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-07-11 04:39:15 +0530

    Simplifying human-friendly ID and data-ID to be internal_id

--- src/main/java/com/mojo/woof/NodeProperties.java
+++ src/main/java/com/mojo/woof/NodeProperties.java
@@ -2,7 +2,7 @@ package com.mojo.woof;
 
 public class NodeProperties {
     public static String ID = "id";
-    public static String HUMAN_READABLE_ID = "human_readable_id";
+    public static String INTERNAL_ID = "internal_id";
     public static String TEXT = "text";
     public static String TYPE = "type";
     public static String NAME = "name";
