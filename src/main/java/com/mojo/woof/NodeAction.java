9932fe350ce7c86a0d8ef01c4499cf7265550211
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-10-02 23:39:15 +0530

    Using logger

--- src/main/java/com/mojo/woof/NodeAction.java
+++ src/main/java/com/mojo/woof/NodeAction.java
@@ -3,16 +3,16 @@ package com.mojo.woof;
 import org.neo4j.driver.Record;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public interface NodeAction {
-    NodeAction JUST_PRINT = new NodeAction() {
-        @Override
-        public ActionResult apply(Record node, List<ActionResult> childResults) {
-            List<String> childStrings = childResults.stream().map(ActionResult::toString).toList();
-            String s = NodeAccess.type(node) + " composed of [" + String.join(",", childStrings) + "]";
-            System.out.println(s);
-            return new SummaryActionResult(s);
-        }
+    java.util.logging.Logger LOGGER = Logger.getLogger(NodeAction.class.getName());
+
+    NodeAction JUST_PRINT = (node, childResults) -> {
+        List<String> childStrings = childResults.stream().map(ActionResult::toString).toList();
+        String s = NodeAccess.type(node) + " composed of [" + String.join(",", childStrings) + "]";
+        LOGGER.info(s);
+        return new SummaryActionResult(s);
     };
 
     ActionResult apply(Record node, List<ActionResult> childResults);
